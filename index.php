833bca018cfc75c1e22dda6da32e6e1da7bafda1
Heet Kalaria <heetkalaria@yahoo.com>
2020-04-28 02:51:25 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,81 @@
+<?php
+    include "logic.php";    
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Covid-19 Dashboard</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://kit.fontawesome.com/996973c893.js"></script>
+</head>
+<body>
+    <div class="container-fluid bg-light p-5 text-center my-3">
+        <h1>Covid-19 Tracker</h1>
+        <h5 class="text-muted">An Open-Source Project Created By Heet Kalaria</h5>
+    </div>
+    <div class="container my-5">
+        <div class="row text-center">
+            <div class="col-md-4 text-warning">
+                <h5>Confirmed</h5>
+                <?php echo $total_confirmed;?>
+            </div>
+            
+            <div class="col-md-4 text-success">
+                <h5>Recovered</h5>
+                <?php echo $total_recoverd;?>
+            </div>
+            
+            <div class="col-md-4 text-danger">
+                <h5>Deaths</h5>
+                <?php echo $total_deaths;?>
+            </div>
+            
+        </div>
+    </div>
+    <div class="container-fluid">
+        <table class="table">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">Countries</th>
+                    <th scope="col">Confirmed</th>
+                    <th scope="col">Recovered</th>
+                    <th scope="col">Deceased</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    foreach($data as $key =>$value)
+                    {
+                        $increase = $value[$days_count]['confirmed'] - $value[$days_count_prev]['confirmed'];
+                ?>
+                    <tr>
+                        <th><?php echo $key;?></th>
+                        <td>
+                            <?php echo $value[$days_count]['confirmed'];?>
+                            <?php if($increase!=0){?>
+                                <small class="text-danger pl-3"><i class="fas fa-arrow-up"></i><?php echo $increase;?></small>
+                            <?php }?>
+                            
+                        </td>
+                        <td><?php echo $value[$days_count]['recovered'];?></td>
+                        <td><?php echo $value[$days_count]['deaths'];?></td>
+                    </tr>
+                <?php
+                    }
+                ?>
+            </tbody>
+        </table>
+    </div>
+
+
+
+    <script src="app.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</body>
+</html>
